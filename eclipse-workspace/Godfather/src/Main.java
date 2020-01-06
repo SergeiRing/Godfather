bf76b1e124c24f311dfcbedb9d2b94db4dd5b8d5
tedaddy <sereshka0967@gmail.com>
2020-01-06 21:54:33 +0300

    update class Main

--- eclipse-workspace/Godfather/src/Main.java
+++ eclipse-workspace/Godfather/src/Main.java
@@ -1,41 +1,18 @@
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.FontFormatException;
-import java.awt.GraphicsEnvironment;
-import java.awt.Toolkit;
-import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-	
-	static String ABSOLUTE_PATH;
+	static ScreenManager screenManager;
 
 	public static void main(String[] args) {
+		Path currentRelativePath = Paths.get("");
+		String path = currentRelativePath.toAbsolutePath().toString();
+		path = path.replaceAll("\\\\", "/");
+		
+		screenManager = new ScreenManager(path);
+		
+		screenManager.getPage("main page");
 
-		try {
-		     GraphicsEnvironment ge = 
-		         GraphicsEnvironment.getLocalGraphicsEnvironment();
-		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(ABSOLUTE_PATH + "/fonts/HollyWoodHills.ttf")));
-		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(ABSOLUTE_PATH + "/fonts/OCR.TTF")));
-		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(ABSOLUTE_PATH + "/fonts/consolas.ttf")));
-		} catch (IOException|FontFormatException e) {
-		     System.out.println(e);
-		}
-
-	}
-	
-	public void exit() {
-		
-	}
-	
-	public void goToAdminPanel() {
-		
-	}
-	
-	public void getGrade() {
-		
 	}
 
 }
