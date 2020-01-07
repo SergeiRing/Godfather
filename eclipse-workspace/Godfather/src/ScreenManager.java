b24f29498dea80cea7186e38615077c11fa980f9
tedaddy <sereshka0967@gmail.com>
2020-01-07 17:52:52 +0300

    del instance of absolute_path from ScreenManager

--- eclipse-workspace/Godfather/src/ScreenManager.java
+++ eclipse-workspace/Godfather/src/ScreenManager.java
@@ -8,37 +8,21 @@ import java.io.IOException;
 import javax.swing.*;
 
 public class ScreenManager {
-	Frame frame;
-	JPanel contentPane;
+	Frame frame  = new Frame();
+	ContentPane contentPane = new ContentPane();
 	
-	String ABSOLUTE_PATH;
+	public ScreenManager() {
 	
-	public ScreenManager(String path) {
-		this.ABSOLUTE_PATH = path;
-		
-		frame = new Frame();
-		contentPane = new JPanel();
 		frame.setContentPane(contentPane);
-		contentPane.setLayout(new CardLayout());
 		
-		try {
-		     GraphicsEnvironment ge = 
-		         GraphicsEnvironment.getLocalGraphicsEnvironment();
-		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(ABSOLUTE_PATH + "/fonts/HollyWoodHills.ttf")));
-		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(ABSOLUTE_PATH + "/fonts/OCR.TTF")));
-		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(ABSOLUTE_PATH + "/fonts/consolas.ttf")));
-		} catch (IOException|FontFormatException e) {
-		     System.out.println(e);
-		}
-		
-		MainPage mainPage = new MainPage(frame.width, frame.height, this);
-		
-		contentPane.add(mainPage, "main page");
+		HomePage home = new HomePage(frame.width, frame.height, this, frame.absolute_path);
+		AdminPanelPage admin = new AdminPanelPage(frame.width, frame.height, this, frame.absolute_path);
+		contentPane.addPage(home, "home");
+		contentPane.addPage(admin, "admin");
 		
 	}
 	
-	public void getPage(String pageName) {
-        CardLayout cardLayout = (CardLayout) contentPane.getLayout();
-        cardLayout.show(contentPane, pageName);
+	public void showPage(String pageName) {
+		contentPane.showPage(pageName);
 	}
 }
