198b9502471c2a04d37c23fa1c8ebe87aba48b37
tedaddy <sereshka0967@gmail.com>
2020-01-08 16:35:37 +0300

    сombine several lines into one

--- eclipse-workspace/Godfather/src/Main.java
+++ eclipse-workspace/Godfather/src/Main.java
@@ -2,10 +2,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-	static ScreenManager screenManager = new ScreenManager();
 
 	public static void main(String[] args) {
-		screenManager.showPage("home");
+		ScreenManager screenManager = new ScreenManager();
 	}
 
 }
